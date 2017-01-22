c826305aad2ec954a1b9f98c2f08f8d202635f07
JenniferLVaccaro <Jennifer.Vaccaro@students.olin.edu>
2017-01-22 12:50:02 -0500

    Updated highway to include car intentions

--- src/Highway.java
+++ src/Highway.java
@@ -20,7 +20,41 @@ public class Highway {
 	}
 	
 	public void simulate(int steps){
-		
+		// UPDATE EACH CAR'S INTENTIONS, i.e. VELOCITY AND LANE CHANGING
+		int columns = highway.size();
+		for(int i=0; i<columns; i++){
+			ArrayList<HighwayGridObject> column = highway.get(i);
+			int lanes = column.size();
+			for(int j=0; j<lanes; j++){
+				HighwayGridObject spot = column.get(j);
+				if(spot.hasCar){
+					Car car = spot.car;
+					int space=0;
+					for(int k=1;k<=Car.MAX_SPEED;k++){
+						if(!highway.get(i).get(j+k).hasCar && highway.get(i).get(j+k).openSpace){
+							space ++;
+						}
+					if(j>0){
+						int Lspace = 0;
+						for(int k=1;k<=Car.MAX_SPEED;k++){
+							if(!highway.get(i).get(j+k).hasCar && highway.get(i).get(j+k).openSpace){
+								space ++;
+							}
+					}
+					if()
+					int Rspace = 0;
+					for(int k=1;k<=Car.MAX_SPEED;k++){
+						if(!highway.get(i).get(j+k).hasCar && highway.get(i).get(j+k).openSpace){
+							space ++;
+						}
+					
+					car.updateVelocity(space);
+				}
+				}
+			}
+		// MOVE EACH CAR BY VELOCITY OR CHANGE ITS LANE
+			
+		}
 	}
 
 }
