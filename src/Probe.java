87e6f004d0aba423a6a70910cb9b8ac314a55f2f
Sidd Singal <sidd@Sidds-MacBook-Pro.local>
2017-01-22 14:26:49 -0500

    starting GUI stuff..a lot of work to be done here

--- src/Probe.java
+++ src/Probe.java
@@ -1,4 +1,22 @@
 
 public class Probe {
+	
+	int carsPassed;
+	int time;
+	
+	public Probe(){
+		carsPassed = 0;
+		time = 0;
+	}
+	
+	public void incCars(){
+		carsPassed++;
+	}
+	
+	public double getThroughput(){
+		return carsPassed * 1.0/time;
+	}
+	
+	
 
 }
